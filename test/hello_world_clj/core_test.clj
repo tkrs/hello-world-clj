d05c860660128ab2ac2ae3094b4fa45301bb5769
Takeru Sato <midium.size@gmail.com>
2015-02-02 03:10:26 +0900

    first commit

---
+++ test/hello_world_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns hello-world-clj.core-test
+  (:require [clojure.test :refer :all]
+            [hello-world-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
